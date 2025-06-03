3c18754226d427e30e8eb21a677485fcb9d6d65e
Rakyrish <mbuguajohn367@gmail.com>
2025-06-03 11:20:03 +0300

    fix the render url

--- config/settings.py
+++ config/settings.py
@@ -182,16 +182,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'PASSWORD': '42162196',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '42162196',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 import os
 import dj_database_url
